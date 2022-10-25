785053f6c192190609b054e9a8c506a5b4499913
darkroom2 <rkomorowski97@gmail.com>
2022-10-25 16:19:32 +0200

    add Model

--- photos/models.py
+++ photos/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Photo(models.Model):
+    title = models.CharField(max_length=100)
+    album_ID = models.IntegerField()
+    width = models.IntegerField()
+    height = models.IntegerField()
+    dominant_color_hex = models.CharField(max_length=7)
+    url = models.CharField(max_length=100)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
