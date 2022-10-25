98e393b1727d180658f60a8de96a79214722728c
darkroom2 <rkomorowski97@gmail.com>
2022-10-25 16:22:05 +0200

    add rest framework

--- django_photos/settings.py
+++ django_photos/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'photos.apps.PhotosConfig',
+    'api.apps.ApiConfig',
 ]
 
 MIDDLEWARE = [
